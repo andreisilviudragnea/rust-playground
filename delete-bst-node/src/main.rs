94e7444e236e06a802db399d7022fdd9e1887094
Andrei-Silviu DRAGNEA (24887) <andrei.dragnea@stud.acs.upb.ro>
2018-04-04 20:28:07 +0300

    Refactoring

--- delete-bst-node/src/main.rs
+++ delete-bst-node/src/main.rs
@@ -15,14 +15,14 @@ impl TreeNode {
     fn from(input: &str) -> Box<TreeNode> {
         let mut input = input.trim().trim_left_matches('[').trim_right_matches(']').split(',');
 
-        let root = match input.next() {
+        let mut root = match input.next() {
             Some(val) => Box::new(TreeNode::new(val.parse().unwrap())),
             None => panic!("no first value")
         };
 
         {
-            let mut queue: VecDeque<&TreeNode> = VecDeque::new();
-            queue.push_back(&*root);
+            let mut queue: VecDeque<&mut TreeNode> = VecDeque::new();
+            queue.push_back(&mut *root);
 
             while !queue.is_empty() {
                 let node = queue.pop_front().unwrap();
@@ -32,9 +32,8 @@ impl TreeNode {
                         match val.trim() {
                             "null" => {}
                             val => {
-                                let left = Box::new(TreeNode::new(val.parse().unwrap()));
-                                unsafe { *(&node.left as *const Option<Box<TreeNode>> as *mut Option<Box<TreeNode>>) = Some(left); }
-                                queue.push_back(node.left.as_ref().unwrap());
+                                node.left = Some(Box::new(TreeNode::new(val.parse().unwrap())));
+                                queue.push_back(node.left.as_mut().unwrap());
                             }
                         }
                     }
@@ -46,9 +45,8 @@ impl TreeNode {
                         match val.trim() {
                             "null" => {}
                             val => {
-                                let right = Box::new(TreeNode::new(val.parse().unwrap()));
-                                unsafe { *(&node.right as *const Option<Box<TreeNode>> as *mut Option<Box<TreeNode>>) = Some(right); }
-                                queue.push_back(node.right.as_ref().unwrap());
+                                node.right = Some(Box::new(TreeNode::new(val.parse().unwrap())));
+                                queue.push_back(node.right.as_mut().unwrap());
                             }
                         }
                     }
@@ -88,23 +86,16 @@ impl TreeNode {
     }
 
     fn find<'a>(parent: &'a Option<Box<TreeNode>>, root: &'a Option<Box<TreeNode>>, key: u32)
-        -> (&'a Option<Box<TreeNode>>, &'a Option<Box<TreeNode>>) {
-        match *root {
-            None => (&None, &None),
-            Some(ref the_root) => {
-                if key < the_root.val {
-                    return TreeNode::find(root, &the_root.left, key);
-                }
-                if key > the_root.val {
-                    return TreeNode::find(root, &the_root.right, key);
-                }
-                return (root, parent);
+                -> (&'a Option<Box<TreeNode>>, &'a Option<Box<TreeNode>>) {
+        if let Some(ref the_root) = *root {
+            if key < the_root.val {
+                return TreeNode::find(root, &the_root.left, key);
+            }
+            if key > the_root.val {
+                return TreeNode::find(root, &the_root.right, key);
             }
         }
-    }
-
-    fn delete_node(tree: &mut TreeNode, key: u32) {
-
+        return (root, parent);
     }
 }
 
